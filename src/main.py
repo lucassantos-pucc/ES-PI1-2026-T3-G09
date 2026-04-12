af3aaaa4656d7cf8a60e29833c3c8ad9106adea9
[Lucas Amaro] <263757077+Luskapaes3@users.noreply.github.com>
2026-04-12 14:10:54 -0300

    Conexao com MySql melhorada
    Antes desse commit, quando a conexao com o MySql nao era efetuada com sucesso, o programa crashava. Agora, criei uma variavel "conectado" booleana que registre se foi conectado ou nao. Tambem adicioneu um try catch para o programa nao quebrar durante a conexao.

--- src/main.py
+++ src/main.py
@@ -5,66 +5,77 @@ from votar import votar
 from votar import ver_informacoes_voto
 from sessao_votacaodb import buscar_sessao_aberta
 
-conexao = conectar()
+conectado = False
 
-if conexao.is_connected():
-    print("Conectado com sucesso!")
+try:
+    conexao = conectar()
+    conectado = True
+except:
+    conectado = False
 
-conexao.close()
+if (conectado):
 
-opcao = ""
+    if conexao.is_connected():
+        print("Conectado com sucesso!")
 
-sessao = buscar_sessao_aberta()
+    conexao.close()
 
-if sessao:
-    sessao_aberta = True
+    opcao = ""
 
-else: 
-    sessao_aberta= False
+    sessao = buscar_sessao_aberta()
+
+    if sessao:
+        sessao_aberta = True
+
+    else:
+        sessao_aberta= False
 
 
-while opcao != "0":
+    while opcao != "0":
 
-    print("\n===== SISTEMA DE VOTAÇÃO =====")
-    print("1 - MESÁRIO")
-    print("2 - ELEITOR")
+        print("\n===== SISTEMA DE VOTAÇÃO =====")
+        print("1 - MESÁRIO")
+        print("2 - ELEITOR")
 
-    if sessao_aberta:
-        print("3 - VOTAR")
-        print("4 - VER VOTO")
+        if sessao_aberta:
+            print("3 - VOTAR")
+            print("4 - VER VOTO")
 
     
 
-    print("0 - SAIR")
+        print("0 - SAIR")
 
-    opcao = input("Escolha uma opção: ")
+        opcao = input("Escolha uma opção: ")
 
-    match opcao:
+        match opcao:
 
-        case "1":
-            menu_mesario()
+            case "1":
+                menu_mesario()
 
-        case "2":
-            menu_eleitor()
+            case "2":
+                menu_eleitor()
 
-        case "3":
-            if sessao_aberta:
-                votar()
-            else:
-                print("A votação ainda não foi aberta pelo mesário.")
+            case "3":
+                if sessao_aberta:
+                    votar()
+                else:
+                    print("A votação ainda não foi aberta pelo mesário.")
         
-        case "4":
-            if sessao_aberta:
-                ver_informacoes_voto()
-            else:
-                print("A votação ainda não foi aberta pelo mesário.")
+            case "4":
+                if sessao_aberta:
+                    ver_informacoes_voto()
+                else:
+                    print("A votação ainda não foi aberta pelo mesário.")
 
       
 
       
 
-        case "0":
-            print("Encerrando sistema")
+            case "0":
+                print("Encerrando sistema")
 
-        case _:
-            print("Opção inválida")
+            case _:
+                print("Opção inválida")
+
+else:
+    print("Nao foi possivel linkar com o MySql")
