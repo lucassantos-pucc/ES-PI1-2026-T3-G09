9af8c5464c656a8f599a6d648c11423c593aff2d
eduardofonseca2 <eduardo.mcf@puccampinas.edu.br>
2026-04-13 23:18:00 -0300

    adicionado alguns codigos  da parte visual do eleitor e fiz a insercao de dados da parte de cadastrar
    coloquei alguns codigos referentes a parte visual do eleitor.(parte amarela do mapa mental) e fiz o codigo da parte de insercao de dados da opcao cadastrar

--- src/menus/menu_eleitor.py
+++ src/menus/menu_eleitor.py
@@ -6,6 +6,66 @@ def menu_eleitor():
     if conexao.is_connected():
         print("Conectado com sucesso no menu eleitor")
 
-    # aqui vai o código do eleitor
+    opcao = ""
 
-    conexao.close()
+    while opcao != "0":
+
+        print("\n===== MENU ELEITOR =====")
+        print("1 - CADASTRAR")
+        print("2 - EDITAR CADASTRO")
+        print("3 - EXCLUIR CADASTRO")
+        print("4 - BUSCAR CADASTRO")
+        print("0 - VOLTAR")
+
+        opcao = input("Escolha uma opção: ")
+
+        match opcao:
+            case "1":
+                cadastrar_eleitor()
+            case "2":
+                editar_eleitor()
+            case "3":
+                excluir_eleitor()
+            case "4":
+                buscar_eleitor()
+            case "0":
+                print("VOLTAR")
+            case _:
+                print("Opção inválida")
+
+
+def cadastrar_eleitor():
+    print("\n===== CADASTRAR ELEITOR =====")
+
+    nome_completo = input("Digite o nome completo: ")
+    cpf = input("Digite o CPF (11 dígitos): ")
+    titulo = input("Digite o título de eleitor: ")
+
+    confirmar = input("Confirmar cadastro? (S/N): ")
+    if confirmar.upper() != "S":
+        print("Cadastro cancelado.")
+        return
+    else:
+        print("Cadastro confirmado!")
+
+#falta fazer a validacao dos dados
+
+def editar_eleitor():
+    print("\n===== EDITAR CADASTRO =====")
+
+# fazer o codigo
+    
+    print("Cadastro atualizado!")
+
+
+def excluir_eleitor():
+    print("\n===== EXCLUIR CADASTRO =====")
+
+    # fazer o codigo
+    print("Cadastro excluído!")
+
+
+def buscar_eleitor():
+    print("\n===== BUSCAR CADASTRO =====")
+
+   # fazer o codigo
