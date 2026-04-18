242d3dc32faf45da389ae7d29dd30577de5037b6
lucasmarassi <142671014+lucasmarassi@users.noreply.github.com>
2026-04-18 10:58:52 -0300

    Delete src/conector/conexao_banco.py

--- src/conector/conexao_banco.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def conectar():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="sistema_votacao"
-    )
